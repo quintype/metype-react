44dd464a0c97dabcce413a6505e42ac3ecf72eb5
Jeevan Kishore <jeevan@quintype.com>
2018-02-02 13:00:05 +0530

    [WIP]

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from 'rollup-plugin-babel';
 export default {
   input: 'index.js',
   output: [
-    {format: "cjs", file: "dist/index.js"},
+    {format: "cjs", file: "dist/index.cjs.js"},
     {format: "es", file: "dist/index.es.js"}
   ],
   plugins: [
