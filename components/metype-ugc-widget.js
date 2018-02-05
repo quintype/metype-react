2973be0a666e4139361bdf026a9428d95ca72a4d
Jeevan Kishore <jeevan@quintype.com>
2018-02-05 12:44:33 +0530

    Add ugc component

---
+++ components/metype-ugc-widget.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {scriptLoader} from "./metype-script-loader";
+
+class MetypeContributionWidget extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.randomNumber = new Date().getMilliseconds();
+  }
+
+  componentDidMount() {
+    !window.talktype && scriptLoader(this.props.host, () => this.initWidget(this.randomNumber));
+    this.initWidget(this.randomNumber);
+  }
+
+  initWidget(randomNumber){
+    if (window.talktype) {
+      window.talktype.initContributionWidget(document.getElementById(`metype-contribution-${randomNumber}`));
+    }
+  }
+
+  render() {
+    const {host, accountId, publisher} = this.props;
+
+    return <div>
+      <div id={`metype-contribution-${this.randomNumber}`}
+           data-metype-account-id={accountId}
+           data-metype-host={host}
+           data-metype-publisher={publisher}>
+      </div>
+    </div>
+  }
+}
+
+export { MetypeContributionWidget };
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export * from './components/metype-feed-widget';
 export * from './components/metype-widget';
 export * from './components/metype-script-loader';
+export * from './components/metype-ugc-widget';
