3bf00b3ca7e11de295d4f2bbf6386b6b6f38fdfc
nandap4790 <nandap4790@gmail.com>
2021-07-02 10:54:49 +0530

    fix: remove docs dependency

--- bin-dev-scripts/standard-version-release.sh
+++ bin-dev-scripts/standard-version-release.sh
@@ -2,14 +2,12 @@
 
 npm install
 git diff --quiet
-npm run docs
 
 BRANCH=$(git symbolic-ref --short HEAD)
 
 if [ "$BRANCH" == 'master' ]
 then
   npx standard-version
-  npx gh-pages -d docs
 else
   npx standard-version --prerelease "$BRANCH" --skip.changelog=true
 fi
